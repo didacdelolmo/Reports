fd54d272654d3caadffe7b242c4e9d02e520a597
Diduhless <didacatz@hotmail.com>
2020-04-09 10:02:18 +0200

    Fix URL mistake on Reports.php

--- src/Reports/Reports.php
+++ src/Reports/Reports.php
@@ -16,7 +16,7 @@ class Reports extends PluginBase {
         $logger = $this->getLogger();
         $logger->info(
             TF::DARK_GRAY . "[" . TF::DARK_AQUA . "Reports by @Diduhless" . TF::DARK_GRAY . "] " .
-            TF::YELLOW . "Reports has been enabled. Updates are released on https://github.com/Diduhless/Reports"
+            TF::YELLOW . "Reports has been enabled. Updates are released on https://github.com/Diduhless/Reports/releases"
         );
     }
 
