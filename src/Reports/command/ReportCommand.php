a74f3d68b951493915e0485108a3f1bd0b284e30
Diduhless <didacatz@hotmail.com>
2020-04-09 09:59:48 +0200

    Clean the code and implement a cooldown for the reports

--- src/Reports/command/ReportCommand.php
+++ src/Reports/command/ReportCommand.php
@@ -6,13 +6,19 @@ namespace Reports\command;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\Player;
 use pocketmine\utils\TextFormat as TF;
 use Reports\Reports;
 
 class ReportCommand extends Command {
 
+    private const REPORT_COOLDOWN = 20;
+
     /** @var Reports */
     private $plugin;
+
+    /** @var int[] */
+    private $reportsCooldown = [];
 
     /**
      * ReportCommand constructor.
@@ -30,7 +36,7 @@ class ReportCommand extends Command {
      */
     public function execute(CommandSender $sender, string $commandLabel, array $args): void {
         if(!isset($args[0]) or !isset($args[1])) {
-            $sender->sendMessage("Usage: " . $this->getUsage());
+            $sender->sendMessage(TF::RED ."Usage: " . $this->getUsage());
             return;
         }
         $server = $this->plugin->getServer();
@@ -40,20 +46,39 @@ class ReportCommand extends Command {
             $sender->sendMessage("This player is not online!");
             return;
         }
+        $name = $player->getName();
+        $cooldown = $this->reportsCooldown[$name];
+
+        if(isset($cooldown) and time() - $cooldown < self::REPORT_COOLDOWN) {
+            $sender->sendMessage(TF::RED . "You can't send multiple reports at once!");
+            return;
+        }
+
         $sender->sendMessage("Your report has been sent.");
+        $this->reportsCooldown[$name] = time();
 
         foreach($server->getOnlinePlayers() as $onlinePlayer) {
             if(!$player->hasPermission("reports.logs")) {
                 continue;
             }
 
-            $onlinePlayer->sendMessage(TF::DARK_GRAY . "***************");
-            $onlinePlayer->sendMessage(TF::DARK_PURPLE . "NEW REPORT");
-            $onlinePlayer->sendMessage(TF::DARK_AQUA . "Reported player: " . TF::GRAY . $player->getName());
-            $onlinePlayer->sendMessage(TF::DARK_AQUA . "Reported by: " . TF::GRAY . $sender->getName());
-            $onlinePlayer->sendMessage(TF::DARK_AQUA . "Reason: " . TF::GRAY . implode(" ", $args));
-            $onlinePlayer->sendMessage(TF::DARK_GRAY . "***************");
+            $this->announceReport($onlinePlayer, $sender, $player, implode(" ", $args));
         }
+    }
+
+    /**
+     * @param Player $admin
+     * @param CommandSender $sender
+     * @param Player $player
+     * @param string $reason
+     */
+    private function announceReport(Player $admin, CommandSender $sender, Player $player, string $reason): void {
+        $admin->sendMessage(TF::DARK_GRAY . "***************");
+        $admin->sendMessage(TF::DARK_PURPLE . "NEW REPORT");
+        $admin->sendMessage(TF::DARK_AQUA . "Reported player: " . TF::GRAY . $player->getName());
+        $admin->sendMessage(TF::DARK_AQUA . "Reported by: " . TF::GRAY . $sender->getName());
+        $admin->sendMessage(TF::DARK_AQUA . "Reason: " . TF::GRAY . $reason);
+        $admin->sendMessage(TF::DARK_GRAY . "***************");
     }
 
 }
